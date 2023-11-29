a51d7c15161a2b3fe5a98e19e5ac3ee291023100
michaelxu12345 <mx21@illinois.edu>
2023-11-29 11:23:28 -0600

    changes - camera into own file, changes to other things - antialiasing

--- includes/color.h
+++ includes/color.h
@@ -12,3 +12,25 @@ void write_color(std::ostream& out, color pixel_color) {
         << static_cast<int>(255.999 * pixel_color.y()) << ' '
         << static_cast<int>(255.999 * pixel_color.z()) << '\n';
 }
+
+void write_color(std::ostream& out, color pixel_color, int samples_per_pixel) {
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // divide color by # of samples
+    auto scale = 1.0 / samples_per_pixel;
+    if (samples_per_pixel == 0) {
+        scale = 1;
+    }
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
+    // write out translated value of color component
+
+    static const interval intensity(0.000, 0.999);
+    out << static_cast<int>(256 * intensity.clamp(r)) << ' '
+        << static_cast<int>(256 * intensity.clamp(g)) << ' '
+        << static_cast<int>(256 * intensity.clamp(b)) << '\n';
+}
--- src/test1.cpp
+++ src/test1.cpp
@@ -1,76 +1,20 @@
 #include "common.h"
-#include "color.h"
-#include "hittable.h"
+#include "camera.h"
 #include "hittable_list.h"
 #include "sphere.h"
 
-#include <iostream>
-
-// using namespace std;
-
-color ray_color(const ray& r, const hittable& world) {
-    hit_record rec;
-    if (world.hit(r, interval(0, infinity), rec)) {
-        return 0.5 * (rec.normal + color(1, 1, 1));
-    }
-
-    vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
-}
-
 int main() {
-    double aspect_ratio = 16.0 / 9.0;
-    int image_width = 1600;
-
-    // Calculate the image height, and ensure that it's at least 1.
-    int image_height = static_cast<int>(image_width / aspect_ratio);
-    image_height = (image_height < 1) ? 1 : image_height;
-
-    // World
-
     hittable_list world;
 
     world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-    world.add(make_shared<sphere>(point3(1, -0.75, -1), 0.2));
-    world.add(make_shared<sphere>(point3(-1, -0.75, -1), 0.2));
-    world.add(make_shared<sphere>(point3(0, -100.9, -1), 100));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
-    // camera
-    double focal_length = 1.0;
-    double viewport_height = 2.0;
-    double viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
-    point3 camera_center = point3(0, 0, 0);
+    camera cam;
 
-    // Calculate the vectors across the horizontal and down the vertical viewport edges.
-    vec3 viewport_u = vec3(viewport_width, 0, 0);
-    vec3 viewport_v = vec3(0, -viewport_height, 0);
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 000;
+    // set to 0 for no antialiasing
 
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    vec3 pixel_delta_u = viewport_u / image_width;
-    vec3 pixel_delta_v = viewport_v / image_height;
-
-    // Calculate the location of the upper left pixel.
-    point3 viewport_upper_left = camera_center
-        - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
-    point3 pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    // Render
-    std::cerr << "Image width height:" << image_width << " " << image_height << std::endl;
-    std::cerr << "Viewport width height:" << viewport_width << " " << viewport_height << std::endl;
-    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
-
-    for (int j = 0; j < image_height; ++j) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i) {
-            point3 pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-            vec3 ray_direction = pixel_center - camera_center;
-            ray r(camera_center, ray_direction);
-
-            color pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color);
-        }
-    }
-
-    std::clog << "\rDone.                 \n";
+    cam.render(world);
 }
